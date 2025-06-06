1500ce3a16fc49a3f3995eb56532638c9678b1af
trindadedev13 <trindadedev13@gmail.com>
2025-06-06 01:24:51 -0300

    initial commit

---
+++ Make.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/bash
+
+# compile
+
+mkdir -p build && cd build
+
+cmake ..
+make
+
+cd ..
+cp build/DAC .
+
+# run it at termux
+if [ "$1" = "--termux" ]; then
+  cp DAC $HOME
+  rm DAC
+
+  cd $HOME
+  chmod +x DAC
+  ./DAC
+fi
---
+++ src/main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+  printf("Welcome do DAC!\n");
+  return 0;
+}
